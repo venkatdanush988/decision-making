2517432a4db2e751bab0aee1b253988e038d8bcd
venkatdanush988 <22hr1a3205@mtieat.org>
2024-09-19 13:39:41 +0530

    Create marksscored.py

---
+++ marksscored.py
@@ -0,0 +1,55 @@
+***
+The newly appointed Vice-Chancellor of Anna University wanted to create an automated grading system for the students to check their grade. When a student enters a mark, the grading system displays the corresponding grade.
+Write a program to solve the given problem.
+  Marks scored  
+  Grade 
+100
+S
+90 - 99
+A
+80 - 89
+B
+70 - 79
+C
+60 - 69
+D
+50 - 59
+E
+<50
+F
+
+Input format:
+The input consists of one integer which corresponds to the marks scored by the student
+
+Output format:
+If a student marks greater than 100, print "Invalid Input". Otherwise, print the grade.
+Sample Input:
+78
+Sample Output:C
+***
+  def get_grade(marks):
+    if marks > 100:
+        return "Invalid Input"
+    elif marks == 100:
+        return "S"
+    elif 90 <= marks < 100:
+        return "A"
+    elif 80 <= marks < 90:
+        return "B"
+    elif 70 <= marks < 80:
+        return "C"
+    elif 60 <= marks < 70:
+        return "D"
+    elif 50 <= marks < 60:
+        return "E"
+    elif marks < 50:
+        return "F"
+    else:
+        return "Invalid Input"
+
+# Input from the user
+try:
+    marks = int(input("Enter marks scored: "))
+    print(get_grade(marks))
+except ValueError:
+    print("Invalid Input")
